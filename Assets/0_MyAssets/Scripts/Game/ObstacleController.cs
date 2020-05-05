87bec0dc9ee19bde2cae6c7b21c3aa80c9b2c428
家子智成 <>
2020-05-06 02:29:07 +0900

    障害物を倒す条件を体力制に変更

--- Assets/0_MyAssets/Scripts/Game/ObstacleController.cs
+++ Assets/0_MyAssets/Scripts/Game/ObstacleController.cs
@@ -24,7 +24,9 @@ public class ObstacleController : MonoBehaviour
 
     public bool Broken(float atk)
     {
-        if (volume > atk) { return false; }
+        volume -= atk;
+        Debug.Log(volume);
+        if (volume > 0) { return false; }
         transform.DOMoveY(10, 1).SetRelative().SetEase(Ease.OutBack);
         transform.DOLocalRotate(new Vector3(180, 0, 180), 0.5f).SetRelative().SetLoops(-1).SetEase(Ease.Linear);
         col.enabled = false;
@@ -34,7 +36,7 @@ public class ObstacleController : MonoBehaviour
         });
         GameManager.i.explosionManager.Explosion(transform);
 
-        Debug.Log(volume);
+
         return true;
     }
 }
