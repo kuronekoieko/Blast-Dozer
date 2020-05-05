e90e86840d7bfd579cf005fa0989cb92c3248052
家子智成 <>
2020-05-05 21:52:56 +0900

    ドラッグで回転させる処理を実装

---
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 mouseDownPos;
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        Controller();
+    }
+
+    void Controller()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = Input.mousePosition - mouseDownPos;
+            direction.z = direction.y;
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+}
